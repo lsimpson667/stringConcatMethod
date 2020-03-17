443744e105e9740b4a71309ae85b4d147eed7faf
Lois Simpson <lsimpson667@gmail.com>
2020-03-16 20:42:45 -0400

    first commit of String Concat Method project

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+let firstName = "Lois ";
+let lastName = "Simpson";
+
+
+function sayMyName(firstName, lastName) {
+    let result = firstName.concat(lastName);
+    alert(result);
+}
+
+console.log(sayMyNameName());
